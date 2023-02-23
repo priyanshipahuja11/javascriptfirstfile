dfa7614f9224d4db0dfd9e5758c08bef835600e9
priyanshipahuja11 <priyanshipahuja11@gmail.com>
2023-02-23 13:32:13 +0530

    first commit

---
+++ js/style.js
@@ -0,0 +1,80 @@
+
+document.write("hello world <br>");
+var a = 5;
+b = 5;
+c = a + b;
+document.write(c + "<br>");
+// var a, b, c;
+// var a = prompt("enter any value of a");
+// b = prompt("enter the value of b");
+// c = a - b;
+// document.write(c + "<br>");
+var a, b, c;
+var a = 6;
+b = 8;
+c = a * b;
+document.write(c + "<br>");
+var a, b, c;
+var a = 8;
+b = 4;
+c = a / b;
+document.write(c + "<br>");
+var a, b, c;
+var a = 8;
+b = 2;
+c = a % b;
+document.write(c + "<br>");
+// var a, b, c;
+// a = 5;
+// b = 3;
+// c = (a && b)
+// document.write(c);
+// var a, b, c;
+// a = 5;
+// b = 3;
+// c = (a || b)
+// document.write(c);
+// var a, b, c;
+// a = 5;
+// b = 3;
+// c = !(a && b)
+// document.write(c);
+var a = 70;
+var b = 90;
+if (a > b) {
+    document.write("true");
+
+}
+else {
+    document.write("false" + "<br>");
+}
+// var a;
+// a = prompt("enter the value of a");
+
+// if (a >= 90) {
+//     document.write("a grade");
+
+// }
+// else if (a >= 60) {
+//     document.write("b grade");
+// }
+// else if (a >= 40) {
+//     document.write("c grade");
+// }
+// else {
+//     document.write("fail");
+// }
+var a, b, c;
+a = 7;
+for (b = 1; b <= 10; b++) {
+    c = a * b;
+    if (c % 2 == 0) {
+        document.write(c + "<br>");
+    }
+    // else {
+    //     document.write(c + "odd" + "<br>");
+    // }
+}
+
+
+
